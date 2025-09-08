1ef07cdb962cd074f5271417b6b61697f78ab627
MarcelloBayuDenar <marcellobayudenarw@gmail.com>
2025-09-08 15:00:45 +0700

    pratikum1 kelas A

---
+++ prakalgo_2025_A_2511532011/src/pekan1/HelloWorld.java
@@ -0,0 +1,21 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//program pertama
+		/* komenmtar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum algo
+		 */
+		  
+		System.out.println("Hello world");
+		System.out.println();
+		System.out.println("Program ini menghasilkan ");
+		System.out.println("tujuh baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("This"+" programs prints a\n" + "quote from the Gettysburg Address" );
+		
+	}
+
+}
---
+++ prakalgo_2025_A_2511532011/src/pekan1/MembuatKue.java
@@ -0,0 +1,32 @@
+package pekan1;
+
+public class MembuatKue {
+
+	public static void main(String[] args) {
+		makeButter();
+		memanggang();
+		memanggang();
+		menghias();
+		
+	}
+	
+	//Langkah 1: siapkan adonan
+	public static void makeButter() {
+		System.out.println("campur bahan kering.");
+		System.out.println("Krim mentega dan gula.");
+		System.out.println("Kocok telurnya.");
+		System.out.println("Masukkan bahan kering.");
+	}
+	// Langkah 2: Panggang sekumpulan kue
+	public static void memanggang() {
+		System.out.println("Setel suhu oven.");
+		System.out.println("Setel pengatur waktu.");
+		System.out.println("Masukkan kue ke dalam oven.");
+		System.out.println("Biarkan cookieuntuk dipanggang.");
+	}
+	//Langkah 3: Hiasi cookie.
+	public static void menghias () {
+		System.out.println("Campur bahan untuk frosting.");
+		System.out.println("Taburkan frosting untuk dan taburan.");
+	}
+}
